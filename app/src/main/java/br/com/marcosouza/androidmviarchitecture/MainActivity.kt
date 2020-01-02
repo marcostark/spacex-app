8d52e31e8c124e2f818a6eea19829232d11feb3a
Marcos Souza <mrcs.stark@gmail.com>
2020-01-01 22:48:57 -0300

    :construction: Implementa layout base da aplicação

--- app/src/main/java/br/com/marcosouza/androidmviarchitecture/MainActivity.kt
+++ app/src/main/java/br/com/marcosouza/androidmviarchitecture/MainActivity.kt
@@ -8,5 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showFragment()
     }
+
+    fun showFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container,
+                MainFragment(), "MainFragment(")
+            .commit()
+    }
+
 }
