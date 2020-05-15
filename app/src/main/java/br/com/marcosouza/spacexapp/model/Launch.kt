66b279276020ed9a7a8d0ff190ec215cd2575fe2
Marcos <mrcs.stark@gmail.com>
2020-05-15 01:47:49 -0300

    feat: :sparkles: Implementa Listagem de lancamentos

--- app/src/main/java/br/com/marcosouza/spacexapp/model/Launch.kt
+++ app/src/main/java/br/com/marcosouza/spacexapp/model/Launch.kt
@@ -1,3 +1,40 @@
 package br.com.marcosouza.spacexapp.model
 
-class Launch
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class Launch (
+    @Expose
+    @SerializedName("flight_number")
+    val flightNumber: Int? = null,
+
+    @Expose
+    @SerializedName("mission_name")
+    val missionName: String? = null,
+
+    @Expose
+    @SerializedName("launch_date_utc")
+    val launchDate: String? = null
+){
+
+    override fun toString(): String {
+        return "Launch(flightNumber=$flightNumber, missionName=$missionName, launchDate=$launchDate)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Launch
+
+        if (flightNumber != other.flightNumber) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return flightNumber ?: 0
+    }
+
+
+}
---
+++ app/src/main/java/br/com/marcosouza/spacexapp/ui/adapter/LaunchListAdapter.kt
@@ -0,0 +1,80 @@
+package br.com.marcosouza.spacexapp.ui.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import br.com.marcosouza.spacexapp.R
+import br.com.marcosouza.spacexapp.model.Launch
+import kotlinx.android.synthetic.main.launch_list_item.view.*
+
+class LaunchListAdapter(private val interaction: Interaction? = null) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Launch>() {
+
+        override fun areItemsTheSame(oldItem: Launch, newItem: Launch): Boolean {
+            return oldItem.flightNumber == newItem.flightNumber
+        }
+
+        override fun areContentsTheSame(oldItem: Launch, newItem: Launch): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    private val differ = AsyncListDiffer(this, DIFF_CALLBACK)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return LaunchViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.launch_list_item,
+                parent,
+                false
+            ),
+            interaction
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is LaunchViewHolder -> {
+                holder.bind(differ.currentList.get(position))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    fun submitList(list: List<Launch>) {
+        differ.submitList(list)
+    }
+
+    class LaunchViewHolder
+    constructor(
+        itemView: View,
+        private val interaction: Interaction?
+    ) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(item: Launch) = with(itemView) {
+            itemView.setOnClickListener {
+                interaction?.onItemSelected(adapterPosition, item)
+            }
+
+            itemView.text_launch_title.text = item.missionName
+//            Glide.with(itemView.context)
+//                .load(item.image)
+//                .into(itemView.blog_image)
+        }
+    }
+
+    interface Interaction {
+        fun onItemSelected(position: Int, item: Launch)
+    }
+}
+
